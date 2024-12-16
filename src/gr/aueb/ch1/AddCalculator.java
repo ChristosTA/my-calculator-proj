2da8feb0a5919758b2d26ae55100c0b300e7075c
Christos Tartaris <xristosbnd@gmail.com>
2024-12-16 19:42:30 +0200

    project created

---
+++ src/gr/aueb/ch1/AddCalculator.java
@@ -0,0 +1,17 @@
+package gr.aueb.ch1;
+
+public class AddCalculator {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 30;
+        int result = 0;
+
+        result= add(num1 , num2);
+
+    }
+
+    static public int add(int a , int b ){
+        return a + b;
+    }
+}
---
+++ src/gr/aueb/ch1/ch2/SubCalculator.java
@@ -0,0 +1,17 @@
+package gr.aueb.ch1.ch2;
+
+public class SubCalculator {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 30;
+        int result = 0;
+
+        result= sub(num1 , num2);
+
+    }
+
+    public static int sub(int a , int b) {
+        return  a - b ;
+    }
+}
